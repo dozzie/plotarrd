c7f715d67c681d3d6931c902fa0b86abc2d0593e
Stanislaw Klekot <dozzie@jarowit.net>
2014-11-26 22:14:38 +0100

    Added thing that will be necessary for sessions.

--- plotarrd/settings.py
+++ plotarrd/settings.py
@@ -14,9 +14,14 @@ IMAGE_STORAGE = 'images'
 
 RRD_PATH = '/var/lib/collectd/rrd'
 
+SECRET_KEY_FILE = 'secret.txt'
+
 #-----------------------------------------------------------------------------
 
 IMAGE_STORAGE_ABS = os.path.join(APP_ROOT, IMAGE_STORAGE)
+
+SECRET_KEY_FILE_ABS = os.path.join(APP_ROOT, SECRET_KEY_FILE)
+SECRET_KEY = open(SECRET_KEY_FILE_ABS).readline().strip()
 
 #-----------------------------------------------------------------------------
 # vim:ft=python
