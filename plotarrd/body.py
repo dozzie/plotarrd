5b7d5dd9f64d37205c3084071afe67983a4c8a82
Stanislaw Klekot <dozzie@jarowit.net>
2014-11-26 23:20:30 +0100

    Added browsing the RRD_PATH directory.

--- plotarrd/body.py
+++ plotarrd/body.py
@@ -37,4 +37,47 @@ def images(image):
         return flask.Response(status = 404)
 
 #-----------------------------------------------------------------------------
+
+@app.route("/browse/add_file")
+def browse_add_file():
+    if 'dir' in flask.request.values:
+        # TODO: sanity check (leading slash, "..")
+        dirname = flask.request.values['dir'].strip('/')
+        dirname_abs = os.path.join(app.config['RRD_PATH'], dirname)
+    else:
+        dirname = ''
+        dirname_abs = app.config['RRD_PATH']
+
+    files = []
+    subdirs = []
+    for e in os.listdir(dirname_abs):
+        if e == '.' or e == '..':
+            continue
+
+        ee = os.path.join(dirname_abs, e)
+        if os.path.isdir(ee):
+            subdirs.append(e)
+        elif e.endswith('.rrd') and os.path.isfile(ee):
+            files.append(e)
+
+    return flask.render_template('browse_add_file.html',
+                                 path = dirname,
+                                 files = sorted(files),
+                                 subdirs = sorted(subdirs))
+
+@app.route("/browse/add_datasource", methods = ["POST", "GET"])
+def browse_add_datasource():
+    if 'file' not in flask.request.values:
+        return flask.redirect(flask.url_for('browse_add_file'))
+
+    # TODO: sanity check (leading slash, "..")
+    filename = flask.request.values['file'].strip('/')
+    filename_abs = os.path.join(app.config['RRD_PATH'], filename)
+
+    datasources = rrd.list_variables(filename_abs)
+
+    return flask.render_template('browse_add_datasource.html',
+                                 file = filename, datasources = datasources)
+
+#-----------------------------------------------------------------------------
 # vim:ft=python:foldmethod=marker
